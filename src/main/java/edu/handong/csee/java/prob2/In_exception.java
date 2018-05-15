ef51878e3110f64be15a15360dc1ae91c6118c2f
Joshua <joshua951013@naver.com>
2018-05-15 12:16:31 +0900

    prob2

--- src/main/java/edu/handong/csee/java/prob2/In_exception.java
+++ src/main/java/edu/handong/csee/java/prob2/In_exception.java
@@ -1,5 +1,34 @@
-package edu.handong.csee.java.prob2;
+package edu.handong.csee.java.prob2;//a new package
+import java.util.*; //imports the following
 
-public class In_exception {
+public class In_exception { //a new class
 
+	private int x = 0, y = 0; //instance variables 
+	public In_exception() // public constructor 
+	{}
+
+	public void error_det() { //a new method 
+		try{ //try statement
+			Scanner keyboard = new Scanner(System.in); //instantiating the scanner as keyboard
+			System.out.print("x: "); //prints the following
+			x = keyboard.nextInt(); //gets in the value from the user to x
+			System.out.print("y: "); //prints the following
+			y = keyboard.nextInt(); //gets in the value from the user to y
+			System.out.println(this.x/this.y); //prints out the x and y values
+		}
+
+		catch(ArithmeticException e){ //catch statement
+
+			System.out.println("java.lang.ArithmeticException: " + e.getMessage()); //prints out the following
+		}
+
+		catch(InputMismatchException e){ //catch statement
+			System.out.println("java.util.InputMismatchException"); //prints out the following
+		}
+
+		catch(Exception e) { //catch statement
+			System.out.println("Some other exception has occurred: " + e.getMessage()); //prints out the following
+		}
+	}
 }
+
--- src/main/java/edu/handong/csee/java/prob2/Inexcep_main.java
+++ src/main/java/edu/handong/csee/java/prob2/Inexcep_main.java
@@ -1,5 +1,11 @@
-package edu.handong.csee.java.prob2;
+package edu.handong.csee.java.prob2;//a package name
 
-public class Inexcep_main {
+public class Inexcep_main { // a new class is made
+	public static void main(String[] args) { //the main method
 
+		int x, y; //local var
+		In_exception obj = new In_exception();// instantiating the in_exception as obj
+		obj.error_det();// calls in the following method from in_exception
+	}
 }
+
