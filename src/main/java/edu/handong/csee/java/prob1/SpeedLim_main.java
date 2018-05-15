1e79efed58c51860fff72d369eb99d8993675508
Joshua <joshua951013@naver.com>
2018-05-15 12:03:14 +0900

    prob1

--- src/main/java/edu/handong/csee/java/prob1/SpeedLim_main.java
+++ src/main/java/edu/handong/csee/java/prob1/SpeedLim_main.java
@@ -1,23 +1,23 @@
-package edu.handong.csee.java.prob1;
-import java.util.Scanner;
-import java.util.Random;
+package edu.handong.csee.java.prob1; //a new package
+import java.util.Scanner; //importing the scanner 
+import java.util.Random;  //importing the random
 
-public class SpeedLim_main {
-	public static void main(String[] args) {
+public class SpeedLim_main { //new class is made
+	public static void main(String[] args) { //the main method is made
 
-		Scanner keyboard = new Scanner(System.in);
-		Random rand = new Random();
-		int limit = 0, speed = 0;
+		Scanner keyboard = new Scanner(System.in); //instantiating the scanner as keyboard
+		Random rand = new Random(); //instantiating the random as rand
+		int limit = 0, speed = 0; //initializing the following var 
 
-		System.out.print("Set the speed limit, officer: ");
+		System.out.print("Set the speed limit, officer: "); //prints out the following
 
-		limit = keyboard.nextInt();
-		speed = rand.nextInt(101);
+		limit = keyboard.nextInt(); //gets in the value from the user to limit
+		speed = rand.nextInt(101); //speed is set randomly in 101
 
-		SpeedLimitter lim = new SpeedLimitter(limit, speed);
-		lim.warnSpeedLimit();
+		SpeedLimitter lim = new SpeedLimitter(limit, speed); //instantiating speedlimitter as lim 
+		lim.warnSpeedLimit(); // calls the following method from speedlimiteer
 
-		keyboard.close();
+		keyboard.close(); // closed
 	}
 }
 
--- src/main/java/edu/handong/csee/java/prob1/SpeedLimitter.java
+++ src/main/java/edu/handong/csee/java/prob1/SpeedLimitter.java
@@ -1,31 +1,31 @@
-package edu.handong.csee.java.prob1;
+package edu.handong.csee.java.prob1; //new package 
 
-public class SpeedLimitter {
+public class SpeedLimitter { //a new class is made
 
-	private int speed = 0;
-	private int limit = 0;
+	private int speed = 0; //instance var
+	private int limit = 0; //instance var
 
-	public SpeedLimitter(int limit, int speed){
+	public SpeedLimitter(int limit, int speed){ //a constructor
 
-		this.limit = limit;
-		this.speed = speed;
+		this.limit = limit; //gets in the limit value as limit
+		this.speed = speed; //gets in the speed value as speed
 	}
 
-	public void warnSpeedLimit(){
+	public void warnSpeedLimit(){ //a new method
 
-		try {
-			if(this.speed > this.limit)
+		try { //try statement
+			if(this.speed > this.limit) //if statement, if the speed is greater than the limit
 			{
-				throw new Exception("Speed Limit " + this.limit + "km exceeded!");
+				throw new Exception("Speed Limit " + this.limit + "km exceeded!"); //prints the following
 			}
 
-			System.out.println("You are a law abiding citizen!");
+			System.out.println("You are a law abiding citizen!"); // if not, prints out the following
 
-		}catch(Exception e) {
-			System.out.println(e.getMessage());
-			System.out.println("You are being fined. ");
+		}catch(Exception e) { //catch statement
+			System.out.println(e.getMessage()); //prints out the following
+			System.out.println("You are being fined. "); //prints out the following
 		}
-		System.out.println("Your current speed: " + this.speed);
+		System.out.println("Your current speed: " + this.speed); //prints out the following
 	}
 }
 
